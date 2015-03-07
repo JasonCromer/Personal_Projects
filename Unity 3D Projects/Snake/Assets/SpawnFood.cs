bdb2241dd32a6c76519c834e14e7aee4e804a42c
JasonCromer <jasonmcromer@gmail.com>
2015-03-06 23:38:42 -0800

    Started and completed a basic snake game with Unity.

---
+++ Unity 3D Projects/Snake/Assets/SpawnFood.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+
+	public GameObject foodPrefab;
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+
+	void Start () 
+	{
+		InvokeRepeating ("Spawn", 3, 4);
+	}
+
+	void Spawn()
+	{
+		// x position between left and right border
+		int x = (int)Random.Range (borderLeft.position.x, borderRight.position.x);
+
+		// y position between left and right border
+		int y = (int)Random.Range (borderBottom.position.y, borderTop.position.y);
+
+		// Instantiate the food at (x,y)
+		Instantiate (foodPrefab, new Vector2 (x, y), Quaternion.identity);
+	}
+
+	
+}
---
+++ Unity 3D Projects/WOD Button/Assets/Scripts/Change_Scene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Change_Scene : MonoBehaviour {
+	
+	public void ChangeToScene (int sceneToChangeTo) {
+		//Application.LoadLevel (sceneToChangeTo);
+
+
+	}
+}
