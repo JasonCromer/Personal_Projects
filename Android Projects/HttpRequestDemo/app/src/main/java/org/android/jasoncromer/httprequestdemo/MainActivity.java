be225c2b65a43f53ba3173e8bf5de92bf8742caa
Jason Cromer <Jasonmcromer@gmail.com>
2015-07-15 16:51:08 -0700

    Started a demo project for sending Http requests to my flask database using AsyncTask

---
+++ Android Projects/HttpRequestDemo/app/src/main/java/org/android/jasoncromer/httprequestdemo/MainActivity.java
@@ -0,0 +1,184 @@
+package org.android.jasoncromer.httprequestdemo;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class MainActivity extends Activity {
+
+    TextView thisTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://10.0.2.2:5000/api/users/2";
+        new httpRequest().execute(url);
+
+    }
+
+
+    private class httpRequest extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            String url1 = url[0];
+            HttpURLConnection httpcon;
+            String data;
+            String result = null;
+
+            try {
+                //Connect
+                httpcon = (HttpURLConnection) ((new URL(url1).openConnection()));
+                httpcon.setDoOutput(true);
+                httpcon.setRequestProperty("Content-Type", "application/json");
+                httpcon.setRequestProperty("Accept", "application/json");
+                httpcon.setRequestMethod("POST");
+                httpcon.connect();
+
+                JSONObject json = new JSONObject();
+                json.put("username", "Michael");
+                json.put("password", "mypass");
+
+                data = json.toString();
+
+                //Write
+                OutputStream os = httpcon.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(data);
+                writer.close();
+                os.close();
+
+                //Read
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpcon.getInputStream(), "UTF-8"));
+
+                String line = null;
+                StringBuilder sb = new StringBuilder();
+
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                br.close();
+                result = sb.toString();
+            }catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), result.toString(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+                HttpURLConnection httpcon;
+                String url = "http://10.0.2.2:5000/api/users";
+                String data = "";
+                String result = null;
+                try {
+                    //Connect
+                    httpcon = (HttpURLConnection) ((new URL(url).openConnection()));
+                    httpcon.setDoOutput(true);
+                    httpcon.setRequestProperty("Content-Type", "application/json");
+                    httpcon.setRequestProperty("Accept", "application/json");
+                    httpcon.setRequestMethod("POST");
+                    httpcon.connect();
+
+
+                    JSONObject json = new JSONObject();
+                    json.put("username", "JasonCromer");
+                    json.put("password", "mypass");
+
+                    data = json.toString();
+
+                    //Write
+                    OutputStream os = httpcon.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(data);
+                    writer.close();
+                    os.close();
+
+                    //Read
+                    BufferedReader br = new BufferedReader(new InputStreamReader(httpcon.getInputStream(), "UTF-8"));
+
+                    String line = null;
+                    StringBuilder sb = new StringBuilder();
+
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    br.close();
+                    result = sb.toString();
+                    Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+ */
