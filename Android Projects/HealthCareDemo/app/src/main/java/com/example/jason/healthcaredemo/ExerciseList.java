dfc17b45d1dcbcbd593b0aa87f88ee2ead94cf05
Jason Cromer <Jasonmcromer@gmail.com>
2015-07-18 19:54:41 -0700

    completed listview with demo pictures and text. Added dynamic listview item loading to eliminate high memory allocation.

--- Android Projects/HealthCareDemo/app/src/main/java/com/example/jason/healthcaredemo/ExerciseList.java
+++ Android Projects/HealthCareDemo/app/src/main/java/com/example/jason/healthcaredemo/ExerciseList.java
@@ -1,78 +1,70 @@
 package com.example.jason.healthcaredemo;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
 
+public class ExerciseList extends Activity {
 
-public class ExerciseList extends Activity{
+    ListView listView;
 
-    private ListView listView;
+    private String[] itemName = {
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War",
+            "Soup",
+            "I Love Dogs",
+            "Basement Forts",
+            "Computer",
+            "App Development",
 
-    private String[] itemNames = {
-            "Exercise1",
-            "Exercise2",
-            "Exercise3",
-            "Exercise4",
-            "Exercise5",
-            "Exercise6",
-            "Exercise7",
-            "Exercise8",
-            "Exercise9",
-            "Exercise10",
-            "Exercise11",
-            "Exercise12",
-            "Exercise13",
-            "Exercise14",
-            "Exercise15",
-            "Exercise16",
-            "Exercise17",
-            "Exercise18",
-            "Exercise19",
-            "Exercise20",
     };
 
+    private Integer[] imageId = {
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8,
+            R.drawable.pic9,
+            R.drawable.pic10,
+            R.drawable.pic11,
+            R.drawable.pic12,
+            R.drawable.pic13,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_list);
 
-        //get listview object from xml
+        CustomListAdapter adapter = new CustomListAdapter(this, itemName, imageId);
         listView = (ListView) findViewById(R.id.listView);
-
-        //define a new adapter
-        //params: (Context, Layout for row, ID of Textview, Array of data)
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, itemNames);
-
-        //Assign adapter to listView
         listView.setAdapter(adapter);
 
-        //ListView item click listener
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                int itemPosition = position;
-
-                String itemValue = (String) listView.getItemAtPosition(position);
-
-                Toast.makeText(getApplicationContext(), "Position :" + itemPosition +
-                " List Item :" + itemValue, Toast.LENGTH_LONG).show();
-
+                String selectedItem = itemName[position];
+                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_LONG).show();
             }
         });
 
     }
+
 }
