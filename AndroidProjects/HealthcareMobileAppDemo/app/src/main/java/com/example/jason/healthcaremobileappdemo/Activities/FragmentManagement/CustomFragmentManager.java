071175173864008c5df0869596c880da6b0d1b33
Jason Cromer <Jasonmcromer@gmail.com>
2015-08-03 14:44:50 -0700

    Cleaned up project

--- AndroidProjects/HealthcareMobileAppDemo/app/src/main/java/com/example/jason/healthcaremobileappdemo/Activities/FragmentManagement/CustomFragmentManager.java
+++ AndroidProjects/HealthcareMobileAppDemo/app/src/main/java/com/example/jason/healthcaremobileappdemo/Activities/FragmentManagement/CustomFragmentManager.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.ViewGroup;
 
 import com.example.jason.healthcaremobileappdemo.Activities.HomePage;
 import com.example.jason.healthcaremobileappdemo.Activities.SettingsActivity;
