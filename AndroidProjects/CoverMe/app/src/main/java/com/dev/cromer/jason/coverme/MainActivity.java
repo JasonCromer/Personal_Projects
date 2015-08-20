189838fc4f09fe6ea01365aeb7ae24cf80df2a3f
Jason Cromer <Jasonmcromer@gmail.com>
2015-08-19 19:58:05 -0700

    Set up the basics of geolocation

--- AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/MainActivity.java
+++ AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/MainActivity.java
@@ -1,22 +1,49 @@
 package com.dev.cromer.jason.coverme;
 
 import android.content.Intent;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.annotation.Annotation;
+
+public class MainActivity extends AppCompatActivity implements PermissionChecker.PermissionResult {
+
+    private static final String[] PERMS_ARRAY = {android.Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int INITIAL_REQUEST = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //requestPermissions(PERMS_ARRAY, INITIAL_REQUEST);
         Intent mapIntent = new Intent(this, MapActivity.class);
         mapIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(mapIntent);
         this.finish();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == INITIAL_REQUEST) {
+            Intent mapIntent = new Intent(this, MapActivity.class);
+            mapIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(mapIntent);
+            this.finish();
+
+        }
+        else {
+            Log.d("TAG", "PERMISSION NOT GRANTED");
+        }
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
 }
--- AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/MapActivity.java
+++ AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/MapActivity.java
@@ -1,92 +1,60 @@
 package com.dev.cromer.jason.coverme;
 
-import android.*;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
+
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements LocationListener, ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMyLocationChangeListener {
+
+public class MapActivity extends FragmentActivity implements com.google.android.gms.location.LocationListener,
+                                                                GoogleApiClient.ConnectionCallbacks,
+                                                                GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
-    private static final String[] INITIAL_PERMS = {android.Manifest.permission.ACCESS_FINE_LOCATION};
-    private static final int INITIAL_REQUEST = 1337;
+    private GoogleApiClient mGoogleApiClient;
+    private Marker mCurrentMarker;
+    private Marker mLastMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        if(!canAccessLocation()) {
-            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
-        }
-        else {
-            setUpMapIfNeeded();
-            Log.d("TAG", "BLAH");
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
         setUpMapIfNeeded();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        Log.d("TAG", "finished request");
-        //setUpMapIfNeeded();
-    }
-
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                //mMap.setMyLocationEnabled(true);
-                //mMap.setOnMyLocationChangeListener(this);
                 setUpMap();
             }
         }
     }
+
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
@@ -95,68 +63,75 @@ public class MapActivity extends FragmentActivity implements LocationListener, A
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMyLocationEnabled(true);
 
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
 
-        try {
-            Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
 
-            if (myLocation != null) {
-                Log.d("TAG", "NOT NULL");
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        showLocation(mCurrentLocation);
+        startLocationUpdates();
 
-                LatLng location = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-                Log.d("VALUES", String.valueOf(location));
-                mMap.addMarker(new MarkerOptions().position(location).title("ITS ME!"));
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 8.0f));
+
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+    protected void showLocation(Location mCurrentLocation) {
+        if (mCurrentLocation != null) {
+            Log.i("Where am I?", "Latitude: " + mCurrentLocation.getLatitude() + ", Longitude:" + mCurrentLocation.getLongitude());
+            mCurrentMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+                    .title("ITS ME!").draggable(true));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 7));
+
+            if(mLastMarker != null) {
+                mLastMarker.setTitle("Expired!");
             }
-            else {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1000, this);
-                Log.d("TAG", "NULL");
-            }
+            mLastMarker = mCurrentMarker;
         }
-        catch (SecurityException se) {
-            Log.d("TAG", "SE CAUGHT");
-            se.printStackTrace();
+
+    }
+
+    protected void startLocationUpdates() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        showLocation(location);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //stop location updates
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            setUpMapIfNeeded();    // <-from previous tutorial
+            startLocationUpdates();
         }
-    }
-
-
-    private boolean canAccessLocation() {
-        int res = getApplicationContext().checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-        return (res == PackageManager.PERMISSION_GRANTED);
-    }
-
-
-
-
-
-
-
-    @Override
-    public void onLocationChanged(Location mlocation) {
-        Log.d("CHANGED", "LOCATION UPDATED");
-
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onMyLocationChange(Location location) {
-        Log.d("TAG", "LOCATION CHANGED");
     }
 }
