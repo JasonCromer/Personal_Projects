76884c2874e20b8d704377799a56d6087ee36142
Jason Cromer <Jasonmcromer@gmail.com>
2015-08-22 08:52:02 -0700

    Created class for HttpPostRequest and parameter object class to pass into the postrequest class

---
+++ AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/Logic/PostRequestParams.java
@@ -0,0 +1,7 @@
+package com.dev.cromer.jason.coverme.Logic;
+
+/**
+ * Created by jason on 8/22/15.
+ */
+public class PostRequestParams {
+}
---
+++ AndroidProjects/CoverMe/app/src/main/java/com/dev/cromer/jason/coverme/Networking/HttpPostRequest.java
@@ -0,0 +1,7 @@
+package com.dev.cromer.jason.coverme.Networking;
+
+/**
+ * Created by jason on 8/22/15.
+ */
+public class HttpPostRequest {
+}
