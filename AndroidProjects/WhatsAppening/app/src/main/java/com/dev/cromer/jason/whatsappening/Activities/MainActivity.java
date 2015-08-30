af83a712f7b893e88205b625c6ab0bf89fd5db9f
Jason Cromer <Jasonmcromer@gmail.com>
2015-08-30 16:38:30 -0700

    Used Preferences Library to store if user agrees to app using location services. If so, request to use maps never pops up again.

--- AndroidProjects/WhatsAppening/app/src/main/java/com/dev/cromer/jason/whatsappening/Activities/MainActivity.java
+++ AndroidProjects/WhatsAppening/app/src/main/java/com/dev/cromer/jason/whatsappening/Activities/MainActivity.java
@@ -1,9 +1,10 @@
 package com.dev.cromer.jason.whatsappening.Activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.v4.content.PermissionChecker;
+import android.support.v4.content.PermissionChecker.PermissionResult;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,19 +12,22 @@ import com.dev.cromer.jason.whatsappening.R;
 
 import java.lang.annotation.Annotation;
 
-public class MainActivity extends AppCompatActivity implements PermissionChecker.PermissionResult {
+public class MainActivity extends AppCompatActivity implements PermissionResult {
 
     private static final String[] PERMS_ARRAY = {android.Manifest.permission.ACCESS_FINE_LOCATION};
-    private static final String permission = "android.Manifest.permission.ACCESS_FINE_LOCATION";
     private static final int INITIAL_REQUEST = 1337;
     private static final int PERMISSION_GRANTED_INT = 0;
+    static boolean hasAcceptedMapAgreement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(getApplicationContext().checkCallingOrSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        hasAcceptedMapAgreement = preferences.getBoolean("hasAccepted", false);
+
+        if(hasAcceptedMapAgreement) {
             startIntent();
         }
         else {
@@ -35,6 +39,12 @@ public class MainActivity extends AppCompatActivity implements PermissionChecker
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(requestCode == INITIAL_REQUEST && grantResults[0] == PERMISSION_GRANTED_INT) {
+
+            //if user grants app request to use location, set hasAccepted to true
+            SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("hasAccepted", true);
+            editor.apply();
             startIntent();
         }
         else {
