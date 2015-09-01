9b1cf2595022f1a2abc332ee92fdc1326c5dba57
Jason Cromer <Jasonmcromer@gmail.com>
2015-08-31 21:29:31 -0700

    Cleaned up main activity

--- AndroidProjects/WhatsAppening/app/src/main/java/com/dev/cromer/jason/whatsappening/Activities/MapActivity.java
+++ AndroidProjects/WhatsAppening/app/src/main/java/com/dev/cromer/jason/whatsappening/Activities/MapActivity.java
@@ -1,18 +1,13 @@
 package com.dev.cromer.jason.whatsappening.Activities;
 
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Geocoder;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -411,35 +406,6 @@ public class MapActivity extends FragmentActivity implements LocationListener,
         showLocation(mCurrentLocation);
         startLocationUpdates();
     }
-
-
-    /*
-        Menu Items
-     */
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_post_new_marker, menu);
-        super.onCreateOptionsMenu(menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 
     @Override
