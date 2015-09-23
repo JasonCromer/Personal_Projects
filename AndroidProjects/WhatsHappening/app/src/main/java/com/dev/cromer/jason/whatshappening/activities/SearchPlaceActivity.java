b48daff7055faeef93df3d3d146ad68f36e84f24
Jason Cromer <Jasonmcromer@gmail.com>
2015-09-23 11:04:15 -0700

    Finished adding navigation buttons

--- AndroidProjects/WhatsHappening/app/src/main/java/com/dev/cromer/jason/whatshappening/activities/SearchPlaceActivity.java
+++ AndroidProjects/WhatsHappening/app/src/main/java/com/dev/cromer/jason/whatshappening/activities/SearchPlaceActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
