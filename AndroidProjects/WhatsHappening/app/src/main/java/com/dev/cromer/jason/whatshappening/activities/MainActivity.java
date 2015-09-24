b321e23d969e964c4b95fc0ed7572d6b0e842a60
Jason Cromer <Jasonmcromer@gmail.com>
2015-09-24 15:30:02 -0700

    Updated verions/cleaned up unused imports

--- AndroidProjects/WhatsHappening/app/src/main/java/com/dev/cromer/jason/whatshappening/activities/MainActivity.java
+++ AndroidProjects/WhatsHappening/app/src/main/java/com/dev/cromer/jason/whatshappening/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.dev.cromer.jason.whatshappening.activities;
 
-import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,8 +15,6 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         startIntent();
     }
-
-
 
     private void startIntent() {
         Intent mapIntent = new Intent(this, MapActivity.class);
