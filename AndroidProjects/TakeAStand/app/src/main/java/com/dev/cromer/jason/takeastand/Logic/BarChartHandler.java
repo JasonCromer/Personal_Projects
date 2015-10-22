1000a2227accd9f400b569a2175d3e9de128a235
Jason Cromer <Jasonmcromer@gmail.com>
2015-10-22 15:42:00 -0700

    Completed barchart activity basics and handler

---
+++ AndroidProjects/TakeAStand/app/src/main/java/com/dev/cromer/jason/takeastand/Logic/BarChartHandler.java
@@ -0,0 +1,130 @@
+package com.dev.cromer.jason.takeastand.Logic;
+
+
+import android.graphics.Color;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BarChartHandler {
+
+    private BarChart barChart;
+    private ArrayList<String> xAxis;
+    protected BarDataSet barDataSet;
+
+    public BarChartHandler(BarChart chart){
+        this.barChart = chart;
+    }
+
+    public void configureBarChartSettings(boolean drawBarShadow, boolean valueAboveBar, boolean pinchZoom,
+                                          boolean gridBackground, boolean legendOn, boolean xValuesOn,
+                                          boolean yValuesOn){
+
+        //Set to bottom position and prevent label skipping
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.getXAxis().setLabelsToSkip(0);
+
+        barChart.setDrawBarShadow(drawBarShadow);
+        barChart.setDrawValueAboveBar(valueAboveBar);
+        barChart.setPinchZoom(pinchZoom);
+        barChart.setDoubleTapToZoomEnabled(pinchZoom);
+
+        barChart.setDrawGridBackground(gridBackground);
+
+        //xAxis
+        barChart.getXAxis().setDrawGridLines(gridBackground);
+        barChart.getXAxis().setEnabled(xValuesOn);
+
+        //yAxis
+        barChart.getAxisLeft().setDrawGridLines(gridBackground);
+        barChart.getAxisRight().setDrawGridLines(gridBackground);
+        barChart.getAxisRight().setEnabled(yValuesOn);
+        barChart.getAxisLeft().setEnabled(yValuesOn);
+
+        barChart.getLegend().setEnabled(legendOn);
+
+    }
+
+    public void setDataSet(int[] data, String dataName){
+        ArrayList<BarEntry> valueSet = new ArrayList<>();
+
+        //Parse through data input and add each value to our ValueSet. Start at 1 to avoid leading zero
+        for(int i = 1; i < data.length; i++){
+            BarEntry value = new BarEntry(data[i], i-1);
+            valueSet.add(value);
+        }
+
+        //Add our completed valueSet to a BarDataSet
+        barDataSet = new BarDataSet(valueSet, dataName);
+        barDataSet.setColors(getReligionColors());
+    }
+
+    public BarDataSet getBarDataSet(){
+        return barDataSet;
+    }
+
+    public void setXAxis(){
+        final String[] religionTypes = getReligionTypes();
+        xAxis = new ArrayList<>();
+
+        //Add all contents of our religionTypes to the xAxis
+        xAxis.addAll(Arrays.asList(religionTypes).subList(0, religionTypes.length));
+    }
+
+    public ArrayList<String> getXAxis(){
+        return xAxis;
+    }
+
+
+
+    public void createBarChart(String description){
+
+        //ensure our x-Axis and data-entries are of the same length
+        if(getBarDataSet().getEntryCountStacks() == getXAxis().size()){
+            BarData data = new BarData(getXAxis(), getBarDataSet());
+            barChart.setData(data);
+            barChart.setDescription(description);
+        }
+        else{
+            Log.d("BARCHART DATA", "xAxis and Dataset are not the same value");
+        }
+    }
+
+
+    public void animateChart(int xValue, int yValue){
+        barChart.animateXY(xValue, yValue);
+        barChart.invalidate();
+    }
+
+
+    private int[] getReligionColors(){
+        return new int[]{
+                Color.rgb(0,127,255),           //Azure
+                Color.rgb(255,0,255),           //Magenta
+                Color.rgb(0,0,255),             //Blue
+                Color.rgb(255,0,127),           //Rose
+                Color.rgb(255,0,0),             //Red
+                Color.rgb(0,255,0),             //Green
+                Color.rgb(255,255,0)            //Yellow
+        };
+    }
+
+    private String[] getReligionTypes(){
+        return new String[] {
+                "Christian",
+                "Islam",
+                "Catholic",
+                "Hindu",
+                "Buddhist",
+                "Agnostic",
+                "Athiest"
+        };
+    }
+}
--- AndroidProjects/TakeAStand/app/src/main/java/com/dev/cromer/jason/takeastand/activities/GlobalStatsActivity.java
+++ AndroidProjects/TakeAStand/app/src/main/java/com/dev/cromer/jason/takeastand/activities/GlobalStatsActivity.java
@@ -3,35 +3,60 @@ package com.dev.cromer.jason.takeastand.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+
+import com.dev.cromer.jason.takeastand.Logic.BarChartHandler;
 import com.dev.cromer.jason.takeastand.Logic.RetrieveStatsHandler;
 import com.dev.cromer.jason.takeastand.R;
 import com.dev.cromer.jason.takeastand.networking.GenericHttpGetRequest;
+import com.github.mikephil.charting.charts.BarChart;
 
 public class GlobalStatsActivity extends AppCompatActivity {
 
+    protected BarChart barChart;
+
+    //constants
     private static final String GET_NUM_RELIGION_TYPE = "http://takeastandapi.elasticbeanstalk.com/get_num_religion_type";
-    protected int[] religionTypeNums;
+    private static final String BAR_CHART_DATA_NAME = "";
+    private static final String BAR_CHART_TITLE = "";
+    private static final int xValueAnimation = 2000;
+    private static final int yValueAnimation = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_stats);
 
-        getReligionTypeData();
+        barChart = (BarChart) findViewById(R.id.barChart);
+        createBarChart();
     }
 
 
-    private void getReligionTypeData(){
+    private void createBarChart(){
+        final int[] religionTypeNumsData = getReligionTypeData();
+        BarChartHandler barChartHandler = new BarChartHandler(barChart);
+
+        //Customize settings
+        barChartHandler.configureBarChartSettings(false, true, false, false, false, true, false);
+
+        //Set the dataSet (yAxis) and xAxis
+        barChartHandler.setDataSet(religionTypeNumsData, BAR_CHART_DATA_NAME);
+        barChartHandler.setXAxis();
+
+        //Use our data to create and animate the chart
+        barChartHandler.createBarChart(BAR_CHART_TITLE);
+        barChartHandler.animateChart(xValueAnimation, yValueAnimation);
+    }
+
+
+    private int[] getReligionTypeData(){
+        final int[] religionTypeNums;
+
         GenericHttpGetRequest httpGetRequest = new GenericHttpGetRequest();
         RetrieveStatsHandler statsHandler = new RetrieveStatsHandler(GET_NUM_RELIGION_TYPE, httpGetRequest);
 
         religionTypeNums = statsHandler.getNumOfReligionTypes();
 
-        //Start at 1 to exclude the default 0 value appended to the start of the array
-        for(int i = 1; i < religionTypeNums.length; i++){
-            Log.d("TAG", String.valueOf(religionTypeNums[i]));
-        }
+        return religionTypeNums;
     }
 
     @Override
