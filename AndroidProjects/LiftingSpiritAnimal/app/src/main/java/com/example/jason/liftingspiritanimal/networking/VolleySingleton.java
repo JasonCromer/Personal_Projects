76add3ccfa843ed354becb7bf7b3099d069afbeb
JasonCromer <jasonmcromer@gmail.com>
2015-12-13 22:05:00 -0800

    Created final build

--- AndroidProjects/LiftingSpiritAnimal/app/src/main/java/com/example/jason/liftingspiritanimal/networking/VolleySingleton.java
+++ AndroidProjects/LiftingSpiritAnimal/app/src/main/java/com/example/jason/liftingspiritanimal/networking/VolleySingleton.java
@@ -3,6 +3,7 @@ package com.example.jason.liftingspiritanimal.networking;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
